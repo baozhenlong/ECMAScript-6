d940108aeead6cccab04b22a2bcaaccadd58eef5
bzl <435801541@qq.com>
2018-09-29 18:03:33 +0800

    目录

---
+++ es6/5-解构-使数据访问更便捷/1-why-using-deconstruction.js
@@ -0,0 +1,12 @@
+//---为何使用解构
+//解构：是一种打破数据结构，将其拆分为更小部分的过程
+
+//从对象和数组中获取特定数据并赋值给变量
+let options = {
+    repeat: true,
+    save: false
+};
+let repeat = options.repeat,
+    save = options.save;
+//如果要提取更多变量，则必须依次编写类似的代码来为变量赋值
+//如果其中还包含
