38ffb8a65625df8616decafaa0d6c138db673a5a
bzl <435801541@qq.com>
2018-11-15 19:52:59 +0800

    静态成员

---
+++ es6/9-JavaScript中类/8-static-member.js
@@ -0,0 +1,37 @@
+//---静态成员
+
+//如果不介意在对象的实例中出现添加的方法和访问器实型，则可以将它们添加到类的原型中
+//如果希望只出现在类中，那么需要使用静态成员
+//在es5及早期版本中，直接将方法添加到构造接函数中来模拟静态成员是一种常见的模式，例如：
+function Person_Type(name) {
+    this.name = name;
+}
+//静态方法
+Person_Type.create = function (name) {
+    return new Person_Type(name);
+};
+//实例方法
+Person_Type.prototype.say_name = function () {
+    console.log("[Person_Type]---name = ", this.name);
+};
+let person = Person.create("damon");
+//在其他编程语言中，由于工厂方法Person_Type.create()使用的数据不依赖Person_Type的实例，因而其会被认为是一个静态方法
+//es6的类语法简化了创建静态成员的过程，在方法或访问器属性名前使用正式的静态注释即可
+//下面这个类等价于之前的示例：
+class Simplify_Person_Type {
+    //等价于Person_Type构造函数
+    constructor(name) {
+        this.name = name;
+    }
+    //等价于Person_Type.prototype.say_name
+    say_name() {
+        console.log("[Simplify_Person_Type]---name = ", this.name);
+    }
+    //等价于Person_Type.create
+    static create(name) {
+        return new Simplify_Person_Type(name);
+    }
+}
+let simplify_person = Simplify_Person_Type.create("damon");
+//类中的所有方法和访问器属性都可以用static关键字来定义，唯一的限制是不能将static用于定义构造函数方法
+//Note：不可在实例中访问静态成员，必须要直接在类中访问静态成员
