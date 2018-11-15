eb52065182e6ca84e9062d01d40c0add96b4c56f
bzl <435801541@qq.com>
2018-11-15 19:52:41 +0800

    生成器方法

--- es6/9-JavaScript中类/7-generator-method.js
+++ es6/9-JavaScript中类/7-generator-method.js
@@ -8,3 +8,27 @@ class My_Class {
         yield 3;
     }
 }
+let instance = new My_Class();
+let iterator = instance.create_iterator();
+//这段代码创建了一个名为My_Class的类，它有一个生成器方法create_iterator()，其返回值为一个硬编码在生成器中的迭代器
+//如果用对象来表示集合，又希望通过简单的方法迭代集合中的值，那么生成器方法就派上用场了
+//数组、Set集合、Map集合为开发者提供了多个生成器方法来与集合中的元素交互
+//尽管生成器方法很实用，但如果类是用来表示值的集合的，那么为它定义一个默认迭代器会更有用
+//通过Symbol.iterator定义生成器方法即可为类定义默认迭代器
+class Collection {
+    constructor() {
+        this.items = [];
+    }
+
+    *[Symbol.iterator]() {
+        // yield* this.items.values();
+        for (let item of this.items) {
+            yield item;
+        }
+    }
+}
+let collection = new Collection();
+collection.items.push(1, 2, 3);
+for (let item of collection) {
+    console.log("item = ", item); //1,2,3
+}
